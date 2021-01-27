23a583c05257d0c5448628a9c6f30f376b5af4e1
KrutiKakkad <krutik@core-assoc.com>
2021-01-27 11:26:07 +0530

    Button Implementation

--- reactdemo/src/App.tsx
+++ reactdemo/src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 import { Currency } from "./componenets/Currecny";
 
 function App() {
+  let value: number = 127.2566,
+    symbol: string = "INR";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Currency value={125.56123} symbol="$" />
+        <Currency value={value} symbol={value > 100 ? "$" : "INR"} />
       </header>
     </div>
   );
--- reactdemo/src/componenets/Currecny/index.tsx
+++ reactdemo/src/componenets/Currecny/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "semantic-ui-react";
+
 interface IProps {
   value: number;
   symbol: string;
@@ -5,9 +7,18 @@ interface IProps {
 
 export const Currency = (props: IProps) => {
   let fixedValue: string = props.value.toFixed(2);
+
+  const childClick = () => {
+    console.log("child click");
+  };
   return (
     <div>
       {fixedValue} {props.symbol}
+      <br></br>
+      <hr></hr>
+      <Button color={props.value > 50 ? "teal" : "orange"} onClick={childClick}>
+        Recalc
+      </Button>
     </div>
   );
 };
